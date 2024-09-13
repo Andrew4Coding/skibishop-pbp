383f1ce9592129b0c2db7f7d42037d28433fbb4f
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-09-13 14:25:18 +0700

    feat: add json and xml view

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from main.views import show_model, create_product_form
+from main.views import show_model, create_product_form, show_all_json, show_all_xml, show_id_json, show_id_xml
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_model, name='show_model'),
     path('add/', create_product_form, name='create_product_form'),
+    path('xml/', show_all_xml, name='show_all_xml'),
+    path('xml/<str:id>/', show_id_xml, name='show_id_xml'),
+    path('json/', show_all_json, name='show_all_json'),
+    path('json/<str:id>/', show_id_json, name='show_id_json'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Product
 from main.forms import ProductEntryForm
 
+from django.http import HttpResponse
+from django.core import serializers
+
 # Create your views here.
 def show_model(request):
     model = Product.objects.all()
-
-    print(model)
 
     example_products = [
         {
@@ -78,3 +79,23 @@ def create_product_form(request):
     
     context = {'form': form}
     return render(request, 'Add/create_product.html', context)
+
+
+def show_all_xml(_):
+    data = Product.objects.all()
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_id_xml(_, id: str):
+    data = Product.objects.filter(id=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_all_json(_):
+    data = Product.objects.all()
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_id_json(_, id: str):
+    data = Product.objects.filter(id=id)
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
