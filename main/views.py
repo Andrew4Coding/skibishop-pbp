7e7867f43dabdbfb5ed4acb927baedfa94ff0827
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-09-17 22:21:05 +0700

    refactor: templates and static folder

--- main/views.py
+++ main/views.py
@@ -9,66 +9,13 @@ from django.core import serializers
 def show_model(request):
     model = Product.objects.all()
 
-    example_products = [
-        {
-            "name": "Bintang Skibidi",
-            "price": 1000,
-            "description": "Skibidi",
-        },
-        {
-            "name": "Doge Coin Plush",
-            "price": 25,
-            "description": "A plush toy of the Doge meme dog",
-        },
-        {
-            "name": "Keanu Reeves Action Figure",
-            "price": 50,
-            "description": "A highly detailed action figure of Keanu Reeves",
-        },
-        {
-            "name": "What Do You Meme? Card Game",
-            "price": 30,
-            "description": "A popular party game based on memes",
-        },
-        {
-            "name": "Fidget Spinner",
-            "price": 10,
-            "description": "A small, handheld toy that spins rapidly",
-        },
-        {
-            "name": "Among Us Merchandise",
-            "price": 15,
-            "description": "A variety of products based on the popular online game Among Us",
-        },
-        {
-            "name": "Squid Game Merchandise",
-            "price": 20,
-            "description": "Items inspired by the hit Netflix series Squid Game",
-        },
-        {
-            "name": "Pet Rock",
-            "price": 5,
-            "description": "A rock painted to look like a pet",
-        },
-        {
-            "name": "Slap Chop",
-            "price": 20,
-            "description": "A hand-powered food chopper that became a viral sensation",
-        },
-        {
-            "name": "The Perpetual Motion Machine",
-            "price": 100,
-            "description": "A device that supposedly operates indefinitely without an external energy source",
-        }
-    ]
-        
     context = {
         'name': 'Andrew Devito Aryo',
         'app_name': 'Skibishop',
         'products': model,
     }
 
-    return render(request, 'main.html', context)
+    return render(request, 'index.html', context)
 
 def create_product_form(request):
     form = ProductEntryForm(request.POST or None)
@@ -78,8 +25,7 @@ def create_product_form(request):
         return redirect('main:show_model')
     
     context = {'form': form}
-    return render(request, 'Add/create_product.html', context)
-
+    return render(request, 'add/create_product.html', context)
 
 def show_all_xml(_):
     data = Product.objects.all()
