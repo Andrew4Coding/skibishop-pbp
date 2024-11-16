b56db46cd564cbcef2a339c27a34a28babc95d69
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-16 20:52:43 +0700

    fix: module name

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from authentication.views import login, register
 from authentication.views import logout
 
-app_name = 'auth'
+app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),
